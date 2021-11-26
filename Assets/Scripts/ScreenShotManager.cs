47e190f52c149b70eef5e4f3021a1247b794f7d6
torcu <45042365+Exater5@users.noreply.github.com>
2021-11-26 13:33:34 +0100

    Finally camera works!

--- Assets/Scripts/ScreenShotManager.cs
+++ Assets/Scripts/ScreenShotManager.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 
 public class ScreenShotManager : MonoBehaviour
 {
-    private Texture2D _targetTexture;
     [SerializeField] private int _resWidth = 1920;
     [SerializeField] private int _resHeight = 1080;
     [SerializeField] private SpriteRenderer sp;
@@ -28,46 +27,17 @@ public class ScreenShotManager : MonoBehaviour
             screenShot.ReadPixels(new Rect(0, 0, _resWidth, _resHeight), 0, 0);
             Camera.main.targetTexture = null;
             RenderTexture.active = null;
+            screenShot.Apply();
             Destroy(rt);
-            byte[] bytes = screenShot.EncodeToPNG();
-            string fileName = $"{Application.dataPath}/Resources/{_currentPhotoIndex}.png";
-            //string fileName = $"{Application.streamingAssetsPath}/{_currentPhotoIndex}.png";
-            System.IO.File.WriteAllBytes(fileName, bytes);
+
+            //byte[] bytes = screenShot.EncodeToPNG();
+            //string fileName = $"{Application.dataPath}/Resources/{_currentPhotoIndex}.png";
+            //System.IO.File.WriteAllBytes(fileName, bytes);
+            //_currentPhotoIndex++;
+            Sprite targetSp = Sprite.Create(screenShot, new Rect(0, 0, screenShot.width, screenShot.height), Vector2.one * 0.5f, 500f);
+            sp.sprite = targetSp;
+            _img.sprite = targetSp;
             _takingPhoto = false;
-            StartCoroutine(LoadSprite(_currentPhotoIndex));
-            _currentPhotoIndex++;
         }
-    }
-
-    IEnumerator LoadSprite(int photoindex)
-    {
-//#if UNITY_EDITOR
-//        UnityEditor.AssetDatabase.Refresh();
-//#endif  
-        yield return new WaitForSeconds(1f);
-        //string path = Application.streamingAssetsPath + "/" + photoindex.ToString();
-        string path = Application.dataPath + "/Resources/" + photoindex.ToString();
-        OnPostprocessTexture(path);
-        _targetTexture = Resources.Load<Texture2D>(path);
-        Sprite targetSp = Sprite.Create(_targetTexture, new Rect(0, 0, 2048, 1024), Vector2.one * 0.5f, 500f);
-        sp.sprite = targetSp;
-        _img.sprite = targetSp;
-    }
-
-    void OnPostprocessTexture(string path)
-    {
-        TextureImporter importer = (TextureImporter)AssetImporter.GetAtPath(path);
-        //importer.textureType = TextureImporterType.GUI;
-
-
-        Object asset = AssetDatabase.LoadAssetAtPath(importer.assetPath, typeof(Texture2D));
-        if (asset)
-        {
-            EditorUtility.SetDirty(asset);
-        }
-        else
-        {
-            importer.textureType = TextureImporterType.GUI;
-        }
-    }
+    }  
 }
